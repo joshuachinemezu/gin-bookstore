1f6406425d514bd3e362478bc7ec9972bca2cab3
Joshua Chinemezu <joshuachinemezu@gmail.com>
2021-03-04 02:26:11 +0400

    Add base models

---
+++ models/book.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+  )
+  
+type Book struct {
+	ID     uint   `json:"id" gorm:"primary_key"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
